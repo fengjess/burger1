41b686ab0bfce09c6362fb6be78ceb0c591446e1
Jessica Feng <jessicafeng14@yahoo.com>
2020-09-15 22:00:28 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # burger
+
+A burger logger that uses MySQL, Node, Express, Handlebars, and ORM.
+
+## Features
+
+* User inputs name of burger
+* Devour, reorder, and delete input
+
+## Screenshots
+
+![Screen Shot 2020-09-15 at 9 51 44 PM](https://user-images.githubusercontent.com/65512016/93293946-1bb59580-f79e-11ea-8f18-048c0cc934df.png)
+
+![Screen Shot 2020-09-15 at 9 52 16 PM](https://user-images.githubusercontent.com/65512016/93293944-1a846880-f79e-11ea-83b5-ac284ce35a73.png)
+
+![Eat-Da-Burger](https://user-images.githubusercontent.com/65512016/93294149-9e3e5500-f79e-11ea-9978-6be95c8377e3.gif)
+
+## URL
+
+https://fengjess-burger.herokuapp.com
+https://github.com/fengjess/burger1
--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,25 @@
-var mysql = require("mysql");
-var connection;
+const mysql = require('mysql');
+
+let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'Helios1019!',
-    database: 'burgers_db'
+    database: 'r5z3bqio7j7oljfc',
   });
-};
+}
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
